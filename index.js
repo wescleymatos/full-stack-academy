636ace852bd9168681f39b781d92f99d984fe1dd
Wescley Matos <wescleymatos@gmail.com>
2017-07-23 12:21:58 -0300

    criação do primeiro template

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 
+//onde estão os templates
+app.set('views', path.join(__dirname, 'views'))
+//tipo de template
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Teste!')
+    res.render('home')
 })
 
 app.listen(port, () => console.log('Server running...'))
